8902fb491c98ab61569a8e2471a76589c6d7d939
ebrub <ebruburhan1@hotmail.com>
2025-04-28 18:11:02 +0300

    add some field required

--- lib/user.dart
+++ lib/user.dart
@@ -6,9 +6,16 @@ import 'package:savespot_project/picture.dart';
 class User extends BaseClass{
   String? firstName;
   String? lastName;
+  String? userName;
   String? password;
+  String? phoneNumber;
+  String? email;
+  DateTime? dateOfBirth;
   EGender? gender;
   Picture? picture; // profil photo
+ 
+
+
 
 
   List<Comment>? commentList;
